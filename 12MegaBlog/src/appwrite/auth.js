653b82839c63fc4a0eb1714964a7f34e1866fdd0
Abhishek Verma <2004abhi.v@gmail.com>
2025-07-16 22:01:49 +0530

    mega blog final work project

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from '../conf/conf';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            // Auto-login after account creation
+            return await this.login({ email, password });
+
+        } catch (error) {
+            console.error("Appwrite service :: createAccount :: error", error);
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.error("Appwrite service :: login :: error", error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Appwrite service :: getCurrentUser :: error", error);
+            return null;
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.error("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import PostCard from "./PostCard";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import Input from "./Input";
+import Login from "./Login";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import AuthLayout from "./AuthLayout"
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  PostCard,
+  Button,
+  PostForm,
+  Input,
+  Login,
+  RTE,
+  Select,
+  Signup,
+  AuthLayout
+};
